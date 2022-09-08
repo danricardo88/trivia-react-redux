a2bdaeb5a7143035a2d8fef496a42de13926d557
jordam mendes <jordammendes@gmail.com>
2022-09-08 15:31:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>YOUR TURN</p>
+        <p>YOUR TURN!</p>
       </header>
     </div>
   );
